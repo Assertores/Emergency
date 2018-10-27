148d87ed64597ff116dd5adf3eccaf3065acd294
(Jam)Maxi <maxi.karnstedt@googlemail.com>
2018-10-27 12:29:08 +0200

    healthbar,PatientController script erstellt

---
+++ Emergency/Assets/scripts/Healthbar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+[RequireComponent(typeof(PatientController))]
+public class Healthbar : MonoBehaviour
+{
+    float MaxHealth = 10f;
+    float currentLife;
+    Image Lifebar;
+
+    public GameObject gameManager;
+
+	void Start ()
+    {
+        Lifebar = this.gameObject.GetComponent<Image>(); 
+        Lifebar.fillAmount = 1;
+    }
+
+    void Update ()
+    {
+		
+	}
+
+    public void SetMaxHealth(float temp)
+    {
+        MaxHealth = temp;
+    }
+
+    void Decrease(float damage)
+    {
+        currentLife -= damage;
+        if(currentLife <= 0)
+        {
+            //tod an patientController
+        }
+    }
+}
---
+++ Emergency/Assets/scripts/PatientController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatientController : MonoBehaviour
+{
+    uint getMoney;
+    Healthbar healthbar;
+
+    void Start ()
+    {
+        healthbar = GetComponent<Healthbar>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+    public void Die()
+    {
+        //geld ausschütten
+        //neuer patient
+        //tod anim
+        //
+    }
+    public void ResetPatient(float life, uint money)
+    {
+        getMoney = money;
+        healthbar.SetMaxHealth(life);
+    }
+}
