f2e9ab72a278e6b9da229acabaa68de6dfda9df3
[JAM]Andreas <andreas.edmeier@web.de>
2018-10-27 16:05:34 +0200

    probieren in datei rein zu schreiben

--- Emergency/Assets/scripts/SaveLoad.cs
+++ Emergency/Assets/scripts/SaveLoad.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class SaveLoad : MonoBehaviour {
@@ -9,6 +10,7 @@ public class SaveLoad : MonoBehaviour {
     [SerializeField] GameObject Krankheiten;
     KrankheitenElement []KE;
     [SerializeField] string Path;
+    [SerializeField] string FileName;
 
     private void Start() {
         gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
@@ -17,11 +19,14 @@ public class SaveLoad : MonoBehaviour {
     }
 
     public void Save() {
-        foreach(KrankheitenElement it in KE) {
+        Directory.CreateDirectory(Path);
+        File.WriteAllText(Path + FileName + ".save", "your realy awesome");
+        File.AppendAllText(Path + FileName + ".save", "you are awesome");
+        foreach (KrankheitenElement it in KE) {
             it.GetCount();//in datei schreiben
         }
         gameManager.GetCurrentMoney(); //in datei schreiben
-        gameManager.GetCurrentDPC(); //in datei schreiben
+        gameManager.GetCurrentDPC().ToString(); //in datei schreiben
     }
     public void Load() {
         //szenenwechsel machen
