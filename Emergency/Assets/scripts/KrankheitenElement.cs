60e7622a649dc12a24fe64acca95dd9bdfa13ab0
[JAM]Andreas <andreas.edmeier@web.de>
2018-10-27 12:15:28 +0200

    Krankheiten und Player angefangen

---
+++ Emergency/Assets/scripts/KrankheitenElement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class KrankheitenElement : MonoBehaviour {
+
+    public GameObject Virus;
+    public String Name;
+    public Image Thumpnail;
+    public uint Kost;
+    public float Dps;
+    public uint Count = 0;
+
+    private void Start() {
+        //die ganze sachen in das prefap schreiben
+    }
+
+    private void Update() {
+        if (Count == 0)
+            return;
+        //mach schaden
+    }
+
+    public void Buy(uint Buy_count = 1) {
+        //is buy able
+        if(Count == 0) {
+            Instantiate(Virus,this.transform);
+        }
+        Count += Buy_count;
+        //darstellung von kosten werden erhöht
+        //darstellung von DPS wird erhöht
+    }
+
+}
---
+++ Emergency/Assets/scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] uint Money;
+    [SerializeField] uint DPC;
+	
+    public void AddMoney(uint amount) {
+        Money += amount;
+    }
+
+    public bool Buy(uint cost) {
+        if (Money < cost)
+            return false;
+        Money -= cost;
+        return true;
+    }
+
+    public void MakeHit() {
+        //deal Damage(DPC)
+    }
+}
