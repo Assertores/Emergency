1c8d2b1b08bff2960037ae73ad0c2a438f3788bf
(Jam)Maxi <maxi.karnstedt@googlemail.com>
2018-10-27 20:28:29 +0200

    audio+button
    Audio slider wird sichtbar wenn button gedrückt wird, unsichtbar beim erneuten Drücken.
    Ordner für Prototyp Audio

--- Emergency/Assets/scripts/SoundManager.cs
+++ Emergency/Assets/scripts/SoundManager.cs
@@ -5,20 +5,38 @@ using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
+    GameObject SliderGo;//gO vom Slider zum aktivieren
     Slider SoundSlider;
+    Button SoundImage;
 
-	void Start ()
+    void Start()
     {
-        SoundSlider = this.gameObject.GetComponent<Slider>();
-        AudioListener.volume = SoundSlider.value;
-	}
-	
-	void Update ()
+        SoundSlider = this.gameObject.GetComponentInChildren<Slider>();
+        SoundSlider.onValueChanged.AddListener(delegate { ValueChanged(); });
+        SoundSlider.value = 1;
+        SliderGo = SoundSlider.gameObject;
+        SliderGo.SetActive(false); SoundImage = this.gameObject.GetComponent<Button>();
+        SoundImage.onClick.AddListener(delegate { OpenSlider(); });
+    }
+
+    void Update ()
     {
-		
-	}
-    public void OnValueChanged()
+    }
+
+    public void ValueChanged()
     {
         AudioListener.volume = SoundSlider.value;
     }
+    void OpenSlider()   //onClick slider sichtbar 
+    {
+        if(SliderGo.activeInHierarchy == true)
+        {
+            SliderGo.SetActive(false);
+        }
+        else
+        {
+            SliderGo.SetActive(true);
+        }
+    }
+
 }
