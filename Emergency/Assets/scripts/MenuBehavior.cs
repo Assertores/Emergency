906985b769b3dac9e2655b88ea8c2c709fb37981
[JAM]Andreas <andreas.edmeier@web.de>
2018-10-28 17:37:58 +0100

    Menu

---
+++ Emergency/Assets/scripts/MenuBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehavior : MonoBehaviour {
+
+    [SerializeField] GameObject Menu;
+
+    private void Start() {
+        Close();
+    }
+
+    public void Open() {
+        Menu.SetActive(true);
+    }
+    public void Close() {
+        Menu.SetActive(false);
+    }
+}
